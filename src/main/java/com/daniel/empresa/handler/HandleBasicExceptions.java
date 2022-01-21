a2713cbec2667e3216c5504d8415ec8d1e44910d
DanielTeruya <daniel.leite.dayo@gmail.com>
2022-01-21 10:15:20 -0400

    Ajustes na exceptions

--- src/main/java/com/daniel/empresa/handler/HandleBasicExceptions.java
+++ src/main/java/com/daniel/empresa/handler/HandleBasicExceptions.java
@@ -5,16 +5,10 @@ import com.daniel.empresa.exception.BasicException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestControllerAdvice
-public class HandleValidationExceptions {
-
+public class HandleBasicExceptions {
 
     @ExceptionHandler(BasicException.class)
     protected ResponseEntity basicExveption(BasicException basic){
